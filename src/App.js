0fcc2f88a3783185d0ae3a66e40da98839eca7b9
leocodeio <basicleo@787>
2024-07-20 10:05:29 +0530

    backup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+// App.js
 import "./App.css";
 import React from "react";
 import Start from "./screens/Start";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import { UserState } from "./context/UserContext";
-
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <UserState>
         <Routes>
           <Route path="/start" element={<Start />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </UserState>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
